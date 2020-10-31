dddc5d744abb42c0f6d056738015f33599c95859
Gi Pyo Kim <gipyok@gmail.com>
2020-10-30 17:20:10 -0700

    Initial Commit

---
+++ HangulClock/HangulClockApp.swift
@@ -0,0 +1,17 @@
+//
+//  HangulClockApp.swift
+//  HangulClock
+//
+//  Created by Gi Pyo Kim on 10/30/20.
+//
+
+import SwiftUI
+
+@main
+struct HangulClockApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
